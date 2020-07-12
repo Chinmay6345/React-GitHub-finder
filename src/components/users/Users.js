e3cbc71c7f874388bafa59ab98c315419e8fd7f8
Chinmay <Chinmay8116@gmail.com>
2020-07-12 16:40:46 +0530

    add spinner

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,19 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
-class Users extends Component {
-  render() {
-    return (
+import Spinner from "../layout/Spinner";
+const users = ({ users, loading }) => {
+  let html = null;
+  if (loading) {
+    html = <Spinner />;
+  } else {
+    html = (
       <div style={userStyle}>
-        {this.props.users.map((user, index) => (
+        {users.map((user, index) => (
           <UserItem key={index} user={user} />
         ))}
       </div>
     );
   }
-}
+  return html;
+};
+
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3,1fr)",
   gridGap: "1rem",
 };
-export default Users;
+export default users;
